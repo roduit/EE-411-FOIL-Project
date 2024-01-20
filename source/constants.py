232885c67bb9c025ce5ef6791ed06bb299fd51d4
Vincent Roduit <101014161+roduit@users.noreply.github.com>
2024-01-20 13:02:22 +0100

    Start project

---
+++ source/constants.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# -*- author : Vincent Roduit -*-
+# -*- date : 2024-01-20 -*-
+# -*- Last revision: 2024-01-20 (Vincent Roduit)-*-
+# -*- python version : 3.11.6 -*-
+# -*- Description: Constants used for this project-*-
+
+#Import libraries
+import os
+import torch
+
+
+#Model constants
+BATCH_SIZE = 128
+TEST_BATCH_SIZE  = 128
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
